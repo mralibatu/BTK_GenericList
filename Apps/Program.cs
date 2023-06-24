4e1eb88d2ab2b38f7711b83ca8fefdc0fff06317
mralibatu <alibatuhan404@gmail.com>
2023-06-24 23:36:01 +0300

    Files added after the lecture

---
+++ Apps/Program.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+
+namespace Apps
+{
+    class Program
+    {
+
+        public static void Main(string[] args)
+        {
+            //var arr = new DataStructers.Array.Array<int>();
+            //var arr = new DataStructers.Array.Array<int>(25,45,60,40,80);
+
+            /*var p1 = new DataStructers.Array.Array<int>(1,2,3,5);
+            var p2 = new int[] {8,9,10};
+            var p3 = new List<int>() { 4,6,45,976};
+            var p4 = new ArrayList() { 12, 65, 47 };    //Getting error due to the fact that ArrayList cant convert to int
+
+            var arr = new DataStructers.Array.Array<int>(p1);*/
+
+            var arr = new DataStructers.Array.Array<int>();
+            for (int i = 0; i < 128; i++)
+            {
+                arr.Add(i+1);
+                Console.WriteLine($"{i+1} has been added to array.");
+                Console.WriteLine($"{arr.Count}/{arr.Capacity}");
+            }
+            Console.WriteLine();
+            for (int i = arr.Count; i >= 1; i--)
+            {
+                Console.WriteLine($"{arr.Remove()} has been removed from the array");
+                Console.WriteLine($"{arr.Count}/{arr.Capacity}");
+            }
+
+            /*arr.Add(23);
+            arr.Add(55);
+            arr.Add(44);
+            arr.Add(55);
+            arr.Add(50);
+            */
+
+            foreach (var item in arr)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.WriteLine("*******************");
+
+            arr.Where(x => x%2 == 0).ToList().ForEach(x => Console.WriteLine(x));
+            //Console.WriteLine($"{arr.Count} / {arr.Capacity}");
+
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ DataStructers/Array/Array.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructers.Array
+{
+    public class Array<T> : IEnumerable<T>,ICloneable
+    {
+        private T[] InnerList;
+        public int Count { get; private set; }
+        public int Capacity => InnerList.Length;
+
+        public Array()
+        {
+            InnerList = new T[2];
+            Count = 0;
+        }
+
+        public Array(params T[] initial)
+        {
+            InnerList = new T[initial.Length];
+            Count = 0;
+            foreach (var item in initial)
+            {
+                Add(item);
+            }
+        }
+        public Array(IEnumerable<T> collection)
+        {
+            InnerList = new T[collection.ToArray().Length];
+            Count = 0;
+            foreach(var item in collection)
+            {
+                Add(item);
+            }
+        }
+
+        public void Add(T item)
+        {
+            if (InnerList.Length == Count)
+            {
+                DoubleArray();
+            }
+            InnerList[Count] = item;
+            Count++;
+        }
+
+        public T Remove()
+        {
+            if(Count == 0)
+                throw new Exception("There is no more item to be removed from the array.");
+
+            if(InnerList.Length/4 == Count)
+            {
+                HalfArray();
+            }
+
+            var temp = InnerList[Count - 1];
+
+            if(Count > 0)
+                Count--;
+
+            return temp;
+        }
+
+        private void HalfArray()
+        {
+            if(InnerList.Length > 2)
+            {
+                var temp = new T[InnerList.Length / 2];
+                System.Array.Copy(InnerList, temp, temp.Length);
+                InnerList = temp;
+            }
+        }
+
+        private void DoubleArray()
+        {
+            var temp = new T[InnerList.Length * 2];
+            System.Array.Copy(InnerList,temp, InnerList.Length);
+            InnerList = temp;
+        }
+
+        public object Clone()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            return InnerList.Take(Count).GetEnumerator();       //If you use InnerList.Select, you get empty indexes too
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
